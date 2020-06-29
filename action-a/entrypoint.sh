78e843ec184e13bc2d406a9d83e81ec87296d539
ajayAC <66760046+ajayAC@users.noreply.github.com>
2020-06-30 05:31:35 +0900

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME.. I am going to greet as $INPUT_MESSAGE.. "
+sh -c "echo Hello world my name is $INPUT_MY_NAME.. I am going to greet as $INPUT_MESSAGE.. Do u know where to meet, it at $INPUT_SECRET_PLACE.. "
